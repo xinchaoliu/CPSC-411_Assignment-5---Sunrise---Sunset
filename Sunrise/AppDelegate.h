70dfff6e5126ed8a6ab0b287a85005b3e89f68ba
Xinchao Liu <leo_liu@csu.fullerton.edu>
2013-05-08 15:23:16 -0700

    Initial Commit

---
+++ Sunrise/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sunrise
+//
+//  Created by Xinchao Liu on 5/8/13.
+//  Copyright (c) 2013 Assignment5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sunrise/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sunrise
+//
+//  Created by Xinchao Liu on 5/8/13.
+//  Copyright (c) 2013 Assignment5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
